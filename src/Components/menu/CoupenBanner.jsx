122951a11baa71d74def7c0badb7762579fe8a06
muhammad haseen <hasinmmpm786@gmail.com>
2024-10-08 22:55:17 +0530

    updated css of past text

--- src/Components/menu/CoupenBanner.jsx
+++ src/Components/menu/CoupenBanner.jsx
@@ -4,9 +4,8 @@ import { ContextList } from "../commen/ContextListProvider";
 import { FaCopy } from "react-icons/fa";
 import Swal from "sweetalert2";
 
-
 const CoupenBanner = () => {
-  const [coupen, setCoupen] = useState([]);  
+  const [coupen, setCoupen] = useState([]);
   const { URL } = useContext(ContextList);
 
   useEffect(() => {
@@ -15,25 +14,28 @@ const CoupenBanner = () => {
         const response = await fetch(`${URL}/coupon/all`);
         const data = await response.json();
         console.log(data);
-        setCoupen(data);  
+        setCoupen(data);
       } catch (error) {
         console.error("Error:", error);
       }
     };
     fetchCoupen();
-  }, [URL]);  
+  }, [URL]);
 
   const copyToClipboard = (code) => {
-    navigator.clipboard.writeText(code).then(() => {
-      Swal.fire({
-        text: `Coupon code copied: ${code}`,
-        icon: "success",
-        timer: 1000,
-        showConfirmButton: false,
+    navigator.clipboard
+      .writeText(code)
+      .then(() => {
+        Swal.fire({
+          text: `Coupon code copied: ${code}`,
+          icon: "success",
+          timer: 1000,
+          showConfirmButton: false,
+        });
+      })
+      .catch((error) => {
+        console.error("Copy failed", error);
       });
-    }).catch((error) => {
-      console.error('Copy failed', error);
-    });
   };
 
   var settings = {
@@ -51,34 +53,39 @@ const CoupenBanner = () => {
     <section className="py-12 ">
       {coupen.length > 0 ? (
         <div className="banner-bg bg-primary pb-6">
-
-        <Slider {...settings}>
-          {coupen.map((coupon, index) => (
-            <div key={index} className=" text-white   w-full">
-              <div className="flex flex-col justify-between items-start px-6 gap-2">
-                
-                <div className="flex flex-col items-start py-14  gap-4 justify-center">
-                  <h1 className="text-lg md:text-xl font-semibold">
-                    Limited Time Offer! Unlock an exclusive{" "}
-                    <span className="px-2 bg-secondary text-primary text-2xl md:text-3xl font-bold">{coupon.discount}% OFF</span><br />
-                    on your next order! Use code{" "}
-                    <span className="px-2 bg-secondary text-primary text-2xl md:text-3xl font-bold">{coupon.code}</span> now.
-                  </h1>
-                  <p className="text-sm md:text-lg italic">
-                    Hurry! This offer won't last long. Enjoy delicious savings today!
-                  </p>
-                </div>
-                <div  onClick={() => copyToClipboard(coupon.code)}  className="cursor-pointer flex justify-center items-center gap-2 bg-secondary text-primary p-3 rounded-xl hover:text-secondary text-lg mx-2">
-
-                Copy Code<FaCopy 
-                  className="" 
-                 
-                />
+          <Slider {...settings}>
+            {coupen.map((coupon, index) => (
+              <div key={index} className=" text-white   w-full">
+                <div className="flex flex-col justify-between items-start px-6 gap-2">
+                  <div className="flex flex-col items-start py-14  gap-4 justify-center">
+                    <h1 className="text-lg md:text-xl font-semibold">
+                      Limited Time Offer! Unlock an exclusive{" "}
+                      <span className="px-2 bg-secondary text-primary text-2xl md:text-3xl font-bold">
+                        {coupon.discount}% OFF
+                      </span>
+                      <br />
+                      on your next order! Use code{" "}
+                      <span className="px-2 bg-secondary text-primary text-2xl md:text-3xl font-bold">
+                        {coupon.code}
+                      </span>{" "}
+                      now.
+                    </h1>
+                    <p className="text-sm md:text-lg italic">
+                      Hurry! This offer won't last long. Enjoy delicious savings
+                      today!
+                    </p>
+                  </div>
+                  <div
+                    onClick={() => copyToClipboard(coupon.code)}
+                    className="cursor-pointer flex justify-center items-center gap-2 hover:bg-black duration-300 bg-secondary text-primary p-3 rounded-xl hover:text-secondary text-lg mx-2"
+                  >
+                    Copy Code
+                    <FaCopy className="" />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
         </div>
       ) : (
         <></>
